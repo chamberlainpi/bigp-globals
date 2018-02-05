191b67bcffe5ab884e761585d37fa42b838eb01e
Pierre Chamberlain <bigp@pierrechamberlain.ca>
2018-02-05 11:07:14 -0400

    Added 'Push / Web Notification' to be able to open the window when it's not currently in focus.

--- public/js/hot-reload.js
+++ public/js/hot-reload.js
@@ -7,6 +7,16 @@ setInterval(function checkConnection() {
 
 $$$.io.on('already-opened', () => {
 	TweenMax.fromTo(document.body, 0.5, {alpha:0}, {alpha:1});
+
+	Push.create("WebApp Already Opened!", {
+		body: "Click here to open it.",
+		icon: '/images/icon-exclamation.png',
+		timeout: 5000,
+		onClick: function () {
+			window.focus();
+			this.close();
+		}
+	});
 });
 
 $$$.io.on('file-changed', path => {
